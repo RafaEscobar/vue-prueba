7ca9ad089c170d6dd4f577da487db15e4bc6ba87
rafa <rescobar@bit.lat>
2024-12-04 11:49:03 -0600

    Agrega componente counter

---
+++ src/components/Counter.vue
@@ -0,0 +1,15 @@
+<script setup>
+    import { ref } from "vue";
+    const counter = ref(0);
+    const plus = () => counter.value++;
+    const minus = () => (counter.value == 0) ? alert("Ho puedes dar más dislikes") : counter.value--;
+</script>
+
+<template>
+    <h1>Likes: {{ counter }}</h1> <br>
+    <p><button @click="plus">Like</button></p>
+    <p><button @click="minus">Dislike</button></p>
+</template>
+
+<style scoped>
+</style>
--- src/main.js
+++ src/main.js
@@ -1,14 +1,23 @@
+//* Importación de los estilos
 import './assets/main.css'
 
+//* Importación de vue
 import { createApp } from 'vue'
+//* Importación de pinia
 import { createPinia } from 'pinia'
 
+//* Importación del archivo App.vue
+// -> Archivo principal .vue
 import App from './App.vue'
+//* Importación del router
 import router from './router'
 
+//* Creación de la aplicación
 const app = createApp(App)
 
+//* Uso de modulos de Pinia y router
 app.use(createPinia())
 app.use(router)
 
+//* Se monta la app en el contenedor id = app de index.html
 app.mount('#app')
